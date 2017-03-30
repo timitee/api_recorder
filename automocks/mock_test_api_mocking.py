585c33ee44961980e3f579132f143529d818dd99
Tim Bushell <tcbushell@gmail.com>
2017-03-31 01:54:43 +0300

    Deleted mocks

--- automocks/mock_test_api_mocking.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-def mock_ghosts_decorators_tests_scenario__ApiMarshall__decorated_m__str_hello_api_who_am_i__4fa1128003eab09e18efe1acfd67483e():
-    return 'ghosts.decorators.tests.scenario.ApiMarshall.decorated_m(Hello Api! Who am I?)'
-    # :endmock:
-
-def mock_ghosts_decorators_tests_scenario__BpiMarshall__decorated_m__str_hello_api_who_am_i__a4f911cfd25b0510e1fcfe00d75152a4():
-    return 'ghosts.decorators.tests.scenario.BpiMarshall.decorated_m(Hello Api! Who am I?)'
-    # :endmock:
