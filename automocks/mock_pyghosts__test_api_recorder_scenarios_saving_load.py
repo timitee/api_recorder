b44d99be4bb2a6ab57ff746c2052a402d62854a9
Tim Bushell <tcbushell@gmail.com>
2017-04-01 16:47:10 +0300

    recording controller tidy up.

--- automocks/mock_pyghosts__test_api_recorder_scenarios_saving_load.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-def mock_ghosts_decorators_tests_scenario__ApiSuperClassDecorated__decorated_super__str_hello_api_who_am_i__a33186ef4fbb8ae7887ece7eb962abf5():
-    return ('ghosts.decorators.tests.scenario.ApiSuperClassDecorated.decorated_super(Hello '
- 'Api! Who am I?)')
-    # :endmock:
--- automocks/mock_pyghosts__test_making_mocks.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-def mock_ghosts_decorators_tests_scenario__ApiMarshall__decorated_m__str_hello_api_who_am_i__f85ea9459bbc1d7da37da6cd0a09da57():
-    return 'ghosts.decorators.tests.scenario.ApiMarshall.decorated_m(Hello Api! Who am I?)'
-    # :endmock:
-
-def mock_ghosts_decorators_tests_scenario__BpiMarshall__decorated_m__str_hello_api_who_am_i__65eefb82a99bce57a607642b649f7ff9():
-    return 'ghosts.decorators.tests.scenario.BpiMarshall.decorated_m(Hello Api! Who am I?)'
-    # :endmock:
