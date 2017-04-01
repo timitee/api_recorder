19c646f2786a43a048c9c785bde0b5f634ed0692
Tim Bushell <tcbushell@gmail.com>
2017-04-01 16:47:51 +0300

    Leave test mocks in place - good documentation.

---
+++ automocks/mock_pyghosts__test_complex_input.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+def mock_ghosts_decorators_tests_test_complex_input____what_can_beat__int_14__int_3__kwarg_a_Dogs__kwarg_b_Dinosaurs__56a612d324c6c9750ed3e3481de1641a():
+    return '14 Dogs can beat 3 Dinosaurs'
+    # :endmock:
+
+def mock_ghosts_decorators_tests_test_complex_input____what_can_beat__int_1__int_13__kwarg_a_Cats__kwarg_b_Mice__8948e29e73b3106cfce3cb8592da2f9d():
+    return '1 Cats can beat 13 Mice'
+    # :endmock:
+
+def mock_ghosts_decorators_tests_test_complex_input____what_can_beat__int_13__int_20__kwarg_a_Cows__kwarg_b_Mice__13f8762c9775538a2cc5df2f0932f1e5():
+    return '20 Cows can beat 13 Mice'
+    # :endmock:
+
+def mock_ghosts_decorators_tests_test_complex_input____what_can_beat__int_10000__int_13__kwarg_a_Ants__kwarg_b_Pigs__a257836bf21ef573bdcf768e499f6ce1():
+    return '10000 Ants can beat 13 Pigs'
+    # :endmock:
